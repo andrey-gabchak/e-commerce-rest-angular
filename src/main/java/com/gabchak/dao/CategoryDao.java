d200dee3f1d7141a84d24fb958a6e1f8e920169b
andrey-gabchak <andrey.gabchak@gmail.com>
2018-11-26 14:19:43 +0200

    CategoryDao findById returned Optional<Category>

--- src/main/java/com/gabchak/dao/CategoryDao.java
+++ src/main/java/com/gabchak/dao/CategoryDao.java
@@ -11,7 +11,7 @@ public interface CategoryDao {
 
     void update(Category category);
 
-    Category findById(Long id);
+    Optional<Category> findById(Long id);
 
     Category findByIdWithProductList(Long id);
 
