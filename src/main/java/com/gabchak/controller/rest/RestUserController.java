712b73a1b714e01673ba14a9984c846ee91d2f0a
gabchak <andrey.gabchak@gmail.com>
2018-11-26 20:03:55 +0200

    Added annotation @CrossOrigin and returned UserDto

--- src/main/java/com/gabchak/controller/rest/RestUserController.java
+++ src/main/java/com/gabchak/controller/rest/RestUserController.java
@@ -1,11 +1,13 @@
 package com.gabchak.controller.rest;
 
+import com.gabchak.controller.external.model.UserDto;
 import com.gabchak.model.User;
 import com.gabchak.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,9 +20,11 @@ public class RestUserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/by-email/{email}")
-    public ResponseEntity<User> getByEmail(@PathVariable String email) {
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/user")
+    public ResponseEntity<UserDto> getByEmail(@RequestParam String email) {
         return userService.getUserByEmail(email)
+                .map(UserDto::of)
                 .map(ResponseEntity::ok)
                 .orElseGet(ResponseEntity.notFound()::build);
     }
