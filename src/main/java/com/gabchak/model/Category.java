77a142b940f00107823d32b3ca86c80e7d393e46
andrey-gabchak <andrey.gabchak@gmail.com>
2018-11-26 12:57:56 +0200

    Removed unnecessary constructors

--- src/main/java/com/gabchak/model/Category.java
+++ src/main/java/com/gabchak/model/Category.java
@@ -1,9 +1,6 @@
 package com.gabchak.model;
 
-import com.gabchak.controller.external.model.CategoryDto;
-
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -13,37 +10,10 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull
     @Column(name = "CATEGORY_NAME")
-    private String name;
-    @OneToMany(mappedBy = "category")
-    private List<Product> products;
-
-    public Category(Long id, String name, List<Product> products) {
-        this.id = id;
-        this.name = name;
-        this.products = products;
-    }
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Category() {
-    }
-
-    public Category(String name) {
-        this.name = name;
-    }
-
-    public static Category of(CategoryDto categoryDto) {
-        return new Category(
-                categoryDto.getId(),
-                categoryDto.getName(),
-                categoryDto.getProducts()
-                );
-    }
+    private String categoryName;
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY) //user field name in category.class
+    private List<Product> productList;
 
     public Long getId() {
         return id;
@@ -53,20 +23,19 @@ public class Category {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getCategoryName() {
+        return categoryName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
     }
 
-    public List<Product> getProducts() {
-        return products;
+    public List<Product> getProductList() {
+        return productList;
     }
 
-    public void setProducts(List<Product> products) {
-        this.products = products;
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
     }
-
 }
