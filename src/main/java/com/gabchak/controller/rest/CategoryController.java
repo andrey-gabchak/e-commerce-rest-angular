59b91051c1402ef6019c5cc9f97c05083cfd1fdc
gabchak <andrey.gabchak@gmail.com>
2019-01-18 22:29:56 +0200

    Fixed uri paths

--- src/main/java/com/gabchak/controller/rest/CategoryController.java
+++ src/main/java/com/gabchak/controller/rest/CategoryController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/rest/categories")
+@RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
--- src/main/java/com/gabchak/controller/rest/ProductController.java
+++ src/main/java/com/gabchak/controller/rest/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/rest/products")
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
