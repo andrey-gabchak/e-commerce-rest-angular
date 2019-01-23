400eb9f973ff1e1c5053ffca156368be163088e6
gabchak <andrey.gabchak@gmail.com>
2019-01-23 15:18:30 +0200

    added method 'find by user'

--- src/main/java/com/gabchak/repositories/CartRepository.java
+++ src/main/java/com/gabchak/repositories/CartRepository.java
@@ -1,7 +1,12 @@
 package com.gabchak.repositories;
 
 import com.gabchak.models.Cart;
+import com.gabchak.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByUser(User user);
 }
