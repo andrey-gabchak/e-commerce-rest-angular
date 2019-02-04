fe01f7b0facad876292da39571315f1dc7ae952e
gabchak <andrey.gabchak@gmail.com>
2019-02-04 21:19:09 +0200

    Removed Cart because frontend have to send order by json

--- src/main/java/com/gabchak/services/dto/mappers/CartMapper.java
+++
@@ -1,8 +0,0 @@
-package com.gabchak.services.dto.mappers;
-
-import ma.glasnost.orika.impl.ConfigurableMapper;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CartMapper extends ConfigurableMapper {
-}
