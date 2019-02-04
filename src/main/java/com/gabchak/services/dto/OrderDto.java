44edfd3aa26b3410688555b79b932f3db851f7ea
gabchak <andrey.gabchak@gmail.com>
2019-02-04 21:10:08 +0200

    Removed second 'id'. Change id type to Integer

--- src/main/java/com/gabchak/services/dto/OrderDto.java
+++ src/main/java/com/gabchak/services/dto/OrderDto.java
@@ -1,7 +1,6 @@
 package com.gabchak.services.dto;
 
 import com.gabchak.models.Product;
-import com.gabchak.models.User;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,9 +9,8 @@ import java.util.Map;
 @Data
 public class OrderDto {
 
-    private int id;
-    private Long orderId;
-    private User customer;
+    private Integer orderId;
+    private UserDto customer;
     private LocalDate orderDate;
     private String orderComment;
     private Double orderAmount;
